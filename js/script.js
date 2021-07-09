aa028912ed3869d2021de27e17b4dffd3cfe5f3f
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-09 14:15:37 -0300

    changed script to save all guests

--- js/script.js
+++ js/script.js
@@ -8,19 +8,17 @@ function getInfoInvitados(number){
         function getMenu(nombre){
             do {
                 menu = prompt('Eliga menu vegetariano o regular, ingrese V o R según correspoda');
-                if(menu != 'V' && menu != 'R'){
+                if(menu == 'V' || menu == 'R'){
+                    if (menu == 'V') {
+                        alert(nombre+' eligió menú vegetariano');
+                    } else {
+                        alert(nombre+' eligió menú regular');
+                    }
+                    document.getElementById("resultados").innerHTML+=`<h4>El nvitado</h4><h6>${nombre}</h6><p>Eligió el menú</p><p>${menu}</P>`;
+                } else {
                     alert(nombre+' se va a quedar sin comer por boludo. Ingrese V para menú vegetariano o R para menú regular');
                 }
-            } while (menu != "V" && menu != "R"){
-                if(menu == 'V'){
-                    alert(nombre+' eligió menú vegetariano');
-                }
-                else {
-                    alert(nombre+' eligió menú regular');
-                }
-            }
-            document.getElementById("resultados").innerHTML=`<h4>El nvitado</h4><h6>${nombre}</h6><p>Eligió el menú</p><p>${menu}</P>`;
-            //Aparece solo el último, resolver.
+            } while (menu != "V" && menu != "R")
         }
         getMenu(nombre);
     }

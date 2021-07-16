c7cf1f623971101312b7926f0d44abd881b4fb8f
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-15 21:47:43 -0300

    worked on scripts

--- js/script-invitados.js
+++ js/script-invitados.js
@@ -1,3 +1,4 @@
+/*
 let cantidadDeInvitados = prompt('Ingrese la cantidad de invitados que quiere confirmar');
 
 function getInfoInvitados(number){
@@ -25,3 +26,54 @@ function getInfoInvitados(number){
 }
 
 getInfoInvitados(cantidadDeInvitados);
+*/
+
+class Invitado{
+    constructor (nombre, apellido, documento, dire, menu, confirma) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = 0;
+        this.dire = '';
+        this.menu = '';
+        this.confirma = '';
+    }
+    comer(){
+        console.log(this.nombre + ' ' + this.apellido + ' comerá el menú ' + this.menu);
+    }
+    mostrarInvitado(){
+        document.getElementById("resultados").innerHTML+=`<h4>Se agregó a ${this.nombre} ${this.apellido} a lista de invitados</h4>`;
+    }
+}
+
+const invitados = [
+    {nombre : "Franco", apellido : "Silva", documento : "39999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Mariana", apellido : "Torreblanca", documento : "38999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Fabricio", apellido : "Silva", documento : "40999999", dire : "", menu : "", confirma : ""},
+    {nombre : "Roxana", apellido : "Podesta", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Sergio", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Cecilia", apellido : "Apellido", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Facundo", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Omar", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Agostina", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Mama de facu", apellido : "Hinojosa", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Katia", apellido : "Itin", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Giovanni", apellido : "Casali", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Joaquín", apellido : "Weiss", documento : "37608601", dire : "Juramento 3675", menu : "Clasico", confirma : "si"}
+];
+
+
+function checkInvitado(){
+    const nombreIngresado = document.getElementById("nombre").value;
+    const filtro = invitados.filter(elemento => nombreIngresado.includes(elemento.nombre) && nombreIngresado.includes(elemento.apellido));
+    console.log(filtro);
+    console.log(nombreIngresado);
+    if(filtro != ""){
+        console.log("Filtro no está vacío");
+    }
+    else {
+        console.log("Filtro está vacío por que esa persona no está invitada");
+    }
+    for (const invitadoFiltrado of filtro) {
+        alert(invitadoFiltrado.nombre + " " + invitadoFiltrado.apellido + " está invitado a la boda de Mariana y Franco");
+    }
+}
--- js/scriptObjeto.js
+++ js/scriptObjeto.js
@@ -1,3 +1,17 @@
+let clave = prompt("Ingrese la clave 1234");
+
+function ingresar(clave){
+    if(clave != "1234"){
+        clave = prompt("La clave no es correcta, ingresela otra vez");
+        ingresar(clave);
+    }
+    else {
+        alert("La clave es correcta");
+    }
+}
+
+ingresar(clave);
+
 class Invitado{
     constructor (nombre, apellido, documento, dire, menu, confirma) {
         this.nombre = nombre;
@@ -15,6 +29,22 @@ class Invitado{
     }
 }
 
+const invitados = [
+    {nombre : "Franco", apellido : "Silva", documento : "39999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Mariana", apellido : "Torreblanca", documento : "38999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Fabricio", apellido : "Silva", documento : "40999999", dire : "", menu : "", confirma : ""},
+    {nombre : "Roxana", apellido : "Podesta", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Sergio", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Cecilia", apellido : "Apellido", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Facundo", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Omar", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Agostina", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Mama de facu", apellido : "Hinojosa", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Katia", apellido : "Itin", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Giovanni", apellido : "Casali", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Joaquín", apellido : "Weiss", documento : "37608601", dire : "Juramento 3675", menu : "Clasico", confirma : "si"}
+];
+
 const invitado1 = new Invitado(document.getElementById("nombre").value, document.getElementById("apellido").value);
 console.log(invitado1);
 
@@ -24,5 +54,7 @@ invitado1.comer();
 
 function agregarInvitado(){
     const invitadoN = new Invitado (document.getElementById("nombre").value, document.getElementById("apellido").value);
+    invitados.push(invitadoN);  //Incluído para desafío clase de Arrays
+    console.log(invitados);
     invitadoN.mostrarInvitado();
 }
